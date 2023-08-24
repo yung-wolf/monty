302ce749c7179b1de4f295131db4c11cc5c09c0f
yung-wolf <edmundbansah@gmail.com>
2023-08-24 11:33:04 -0700

    fixed betty style

--- pop.c
+++ pop.c
@@ -5,7 +5,7 @@
  * @stack: stack
  * @line_number: line number
  */
-void pop (stack_t **stack, unsigned int line_number)
+void pop(stack_t **stack, unsigned int line_number)
 {
 	if (!stack || !*stack)
 	{

adce4f134aa5eb1b30f67db62dc894ff09ca2efa
Edmund Eli Bansah <edmundbansah@gmail.com>
2023-08-28 09:29:18 +0000

    fix pint errors

--- _stack.c
+++ _stack.c
@@ -91,7 +91,7 @@ void pint(stack_t **stack, unsigned int line_number)
 {
 	if (!stack || !*stack)
 	{
-		fprintf(stderr, "L%u: can't pint, empty stack\n", line_number);
+		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	printf("%d\n", (*stack)->n);
